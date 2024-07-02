d1b2918b597110daad36926ff4b8e7df57032b05
Man From Existence <89142466+manfromexistence@users.noreply.github.com>
2024-07-02 23:53:58 +0000

    feat: customiezed github codespace and downloaded so many extentions

--- rust/helloProject/src/main.rs
+++ rust/helloProject/src/main.rs
@@ -200,17 +200,36 @@ fn there_can_be_only_one_owner_at_a_time(){
 // 1. Immutable Reference: &T
 // 2. Mutable Reference: &mut T
 // Create a reference of a by using the & operator.
+// So, you can only have one mutable reference or many immutable references.
 fn create_a_reference(){
-    let _x: i32 = 5;
+    let mut _x: i32 = 5;
     // let r: i32 = _x; // This is not good cause here we are taking ownership of x totally so x wil no longer be available anymore.
-    let _r: &i32 = &_x; // This is good cause we are borrowing x. But notice that we no saying anything about mutability.
+    let _r: &mut i32 = &mut _x; // This is good cause we are borrowing x. But notice that we no saying anything about mutability.
     
     *_r += 1;
+    *_r -= 3;
 
-    println!("Value of x is {}", _x);
-    println!("Value of r is {}", _r);
+    println!("Value of _x is {}", _x);
+    // println!("Value of _r is {}", _r);
 }
 
 
+// ===============================================================================================================================================
+// Stucts are also in C and C++ code.
+struct BankAccount {
+    owner: String,
+    balance: f64,
+}
+
+impl BankAccount {{
+    fn withdraw(&mut self, amount: f64) {
+        println!("Withdrawing {} from account owned by {}", amount, self.owner);
+        seft.balance -= amount;
+    }
+
+    fn check_balance(&self){
+        println!("Account owned by {} has a balance of {}", self.owner, self.balance);
+    }
+}
 
 
