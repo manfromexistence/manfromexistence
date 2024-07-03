1d777a46601b084bf52eb5764ba10140b7362964
Man From Existence <89142466+manfromexistence@users.noreply.github.com>
2024-07-03 02:08:48 +0000

    feat: Leaned if-else and else-if

--- rust/helloProject/src/main.rs
+++ rust/helloProject/src/main.rs
@@ -136,6 +136,7 @@ fn main() {
         4. etc...
     */
     if_else();
+    else_if();
 }
 
 //const _X = {
@@ -347,3 +348,15 @@ fn if_else(){
     }
 }
 
+fn else_if(){
+    let number: u8 = 6;
+    if number % 4 == 0 {
+        println!("The number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("The number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("The number is divisible by 2");
+    } else {
+        println!("The number is not divisible by 4,3,2");
+    }
+}
