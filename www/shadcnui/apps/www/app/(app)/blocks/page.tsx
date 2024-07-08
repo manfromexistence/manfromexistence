b5c7b26d42949aab8625cb2692ae80e2373ce990
Harry <ajju40959@gmail.com>
2024-07-08 09:47:20 +0000

    feat: new shadcnui charts

---
+++ www/shadcnui/apps/www/app/(app)/blocks/page.tsx
@@ -0,0 +1,12 @@
+import { getAllBlockIds } from "@/lib/blocks"
+import { BlockDisplay } from "@/components/block-display"
+
+export default async function BlocksPage() {
+  const blocks = (await getAllBlockIds()).filter(
+    (name) => !name.startsWith("chart")
+  )
+
+  return blocks.map((name, index) => (
+    <BlockDisplay key={`${name}-${index}`} name={name} />
+  ))
+}
