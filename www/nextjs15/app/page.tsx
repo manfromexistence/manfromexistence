6c72634237c8209059816b376fae1f92a0161bbb
Harry <ajju40959@gmail.com>
2024-07-08 09:07:21 +0000

    feat: cannot use nextjs15 with tailwindcss4

--- www/nextjs15/app/page.tsx
+++ www/nextjs15/app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <div>Hello Nextjs with VS code good enough font!</div>
+    <div className="h-screen w-full text-center">Hello Nextjs with VS code good enough font!</div>
   );
 }
