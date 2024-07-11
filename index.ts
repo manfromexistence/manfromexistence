75aad00a2d754637b84723399e7504ddb4d59aec
manfromexistence02 Emon <manfromexistence02@gmail.com>
2024-07-11 16:14:00 +0000

    feat: I am finding it difficult to download youtube playlist

--- index.ts
+++ index.ts
@@ -3,3 +3,33 @@ const ytdl = require('ytdl-core');
 
 ytdl('http://www.youtube.com/watch?v=aqz-KE-bpKQ')
     .pipe(fs.createWriteStream('video.mp4'));
+
+// const ytlist = require('youtube-playlist');
+ 
+// const url = 'https://www.youtube.com/playlist?list=PLWKjhJtqVAbnZtkAI3BqcYxKnfWn_C704';
+ 
+// ytlist(url, 'url').then((res: any) => {
+//   console.log(res);
+//   /* Object
+//   { data:
+//    { playlist:
+//       [ 'https://youtube.com/watch?v=bgU7FeiWKzc',
+//         'https://youtube.com/watch?v=3PUVr8jFMGg',
+//         'https://youtube.com/watch?v=3pXVHRT-amw',
+//         'https://youtube.com/watch?v=KOVc5o5kURE' ] } }
+//    */
+// });
+
+var PLD = require('youtube-playlist-download');
+
+
+//usage node dl.js [playlist_id]
+//e.g.  node dl.js PL8BFA7E713C650560
+var output_dir    = "dl",
+     playlist_id  = process.argv[2];
+var pld       = new PLD("PL8BFA7E713C650560", output_dir);
+
+
+pld.download(function(){
+  console.log("All done, have a great day !");
+});
--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,36 @@
 //     println!("Hello, world!");
 // }
 
-use std::io;
+// use std::io;
 
-fn main() {
-    let mut video_link = String::new();
+// fn main() {
+//     let mut video_link = String::new();
 
-    println!("Enter a YouTube video link:");
-    io::stdin().read_line(&mut video_link).expect("Error reading input");
+//     println!("Enter a YouTube video link:");
+//     io::stdin().read_line(&mut video_link).expect("Error reading input");
 
-    // Simulate checking for download permission (replace with actual logic)
-    println!("Checking download permission for: {}", video_link);
-    let is_download_permitted = false; // Assume permission is denied
+//     // Simulate checking for download permission (replace with actual logic)
+//     println!("Checking download permission for: {}", video_link);
+//     let is_download_permitted = false; // Assume permission is denied
 
-    if is_download_permitted {
-        println!("Download is permitted, but functionality is not implemented for ethical reasons.");
-        // Replace with actual download logic if implemented ethically (e.g., using YouTube Data API)
-    } else {
-        println!("Download is not permitted.");
+//     if is_download_permitted {
+//         println!("Download is permitted, but functionality is not implemented for ethical reasons.");
+//         // Replace with actual download logic if implemented ethically (e.g., using YouTube Data API)
+//     } else {
+//         println!("Download is not permitted.");
+//     }
+// }
+
+use rusty_ytdl::Video;
+
+#[tokio::main]
+async fn main() {
+    let video_url = "https://www.youtube.com/watch?v=FZ8BxMU3BYc"; // You can replace this with your playlist URL
+    let video = Video::new(video_url).unwrap();
+    let stream = video.stream().await.unwrap();
+
+    while let Some(chunk) = stream.chunk().await.unwrap() {
+        // Do what you want with chunks (e.g., save to a file)
+        println!("{:#?}", chunk);
     }
 }
