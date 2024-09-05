93d77599d3d28571804ecab5152e613806e4aa7a
manfromexistence <ajju40959@gmail.com>
2024-09-05 15:00:17 +0000

    feat: new www

--- www/next.config.mjs
+++ www/next.config.mjs
@@ -1,4 +1,34 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'images.unsplash.com',
+            port: '',
+            pathname: '/**',
+          },
+          {
+            protocol: 'https',
+            hostname: 'seo-heist.s3.amazonaws.com',
+            port: '',
+            pathname: '/**',
+          },
+          {
+            protocol: 'https',
+            hostname: 'github.com',
+            port: '',
+            pathname: '/**',
+          },
+          {
+            protocol: 'https',
+            hostname: 'utfs.io',
+            port: '',
+            pathname: '/**',
+          },
+        ],
+      },
+    
+};
 
 export default nextConfig;
