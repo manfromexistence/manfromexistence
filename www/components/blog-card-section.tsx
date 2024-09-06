01da6789d4f343271fe9913e12e1c1d51ab1e41e
manfromexistence <ajju40959@gmail.com>
2024-09-06 10:51:19 +0000

    feat: hello

--- www/components/blog-card-section.tsx
+++ www/components/blog-card-section.tsx
@@ -4,11 +4,10 @@ import { blogs } from '@/content/article'
 import { Separator } from './ui/separator'
 
 export default function BlogCardSection() {
-
   return (
     <div className='my-[2rem] w-full'>
       <div className='flex flex-col items-start justify-center w-full mb-4 gap-2'>
-        <h1 className='text-xl font-medium tracking-tight'>Checkout These Blog Posts</h1>
+        <h1 className='text-xl font-medium tracking-tight'>Social Media</h1>
         <Separator />
       </div>
       <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
@@ -18,6 +17,9 @@ export default function BlogCardSection() {
           </Link>
         ))}
       </div>
+      <Link className='border rounded-md flex w-full py-2 items-center justify-center my-4' target='_blank' href={'https://google.com'} >
+        See all social media contents
+      </Link>
     </div>
   )
 }
