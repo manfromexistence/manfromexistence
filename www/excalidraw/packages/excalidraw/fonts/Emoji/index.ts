c89ee6cf40fb77c0d6c4e92c6b13a61a7f5ca6af
manfromexistence04 <167656180+manfromexistence04@users.noreply.github.com>
2025-03-06 04:45:39 +0000

    chore: remove unused files and clean up the excalidraw directory

--- www/excalidraw/packages/excalidraw/fonts/Emoji/index.ts
+++
@@ -1,9 +0,0 @@
-import { LOCAL_FONT_PROTOCOL } from "../FontMetadata";
-
-import { type ExcalidrawFontFaceDescriptor } from "../Fonts";
-
-export const EmojiFontFaces: ExcalidrawFontFaceDescriptor[] = [
-  {
-    uri: LOCAL_FONT_PROTOCOL,
-  },
-];
--- www/excalidraw/packages/excalidraw/fonts/Virgil/index.ts
+++
@@ -1,9 +0,0 @@
-import Virgil from "./Virgil-Regular.woff2";
-
-import { type ExcalidrawFontFaceDescriptor } from "../Fonts";
-
-export const VirgilFontFaces: ExcalidrawFontFaceDescriptor[] = [
-  {
-    uri: Virgil,
-  },
-];
