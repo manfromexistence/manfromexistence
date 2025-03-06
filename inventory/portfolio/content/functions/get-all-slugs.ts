2c07928fe078cf5c61938b3e1ed630be2a2636c0
manfromexistence04 Emon <manfromexistence04@gmail.com>
2025-03-06 07:08:34 +0000

    feat: hello

--- inventory/portfolio/content/functions/get-all-slugs.ts
+++
@@ -1,7 +0,0 @@
-import { blogs } from "../article";
-
-export function getAllBlogSlugs() {
-  return blogs.map(blog => ({
-    params: { slug: blog.slug || blog.name.toLowerCase().replace(/ /g, '-') }
-  }));
-}
